1c7d6c37664a4ee8df5f79c911b5e237a672d6c1
Dmitrii Nikolskii <dmnikolskii@gmail.com>
2021-08-10 19:37:07 +0600

    logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import {ReactComponent as Logo} from './Pepsico_logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Container">
+          <Logo className="logo"></Logo>
           <h1>Алиев Ж.О.</h1>
           <textarea className="TaskDescription" placeholder="Опишите планируемую задачу"></textarea>
           <button className="Button Green" disabled={true}>Старт</button>
